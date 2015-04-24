5a09da58c429e5c6fe5bfc568e8e4c8161cfee18
Vitosh <vitosh.doynov@gmail.com>
2015-04-24 14:39:18 +0300

    update2

--- Week_6/server.js
+++ Week_6/server.js
@@ -2,18 +2,18 @@ var express = require('express');
 var app = express(); 
 var fs = require("fs");
  
-app.use(express.static('public'));
 app.set('view engine', 'jade');
- 
+
+// routes
 app.get('/', function (req, res) {
-  res.render('index'), {products: products};
+  res.render('index');
 })
- 
+
 // listen for files: /post -> /views/post.jade
 app.get("/:fileName", function(req, res, next){
   if(req.params && req.params.fileName){
     var fileName = req.params.fileName.replace(".html","");
- 
+
     // if jade file exists
     if(fs.existsSync(__dirname+"/views/"+fileName+".jade")){
       res.render(fileName);
@@ -21,12 +21,12 @@ app.get("/:fileName", function(req, res, next){
     } else {
       next();
     }
- 
+
   } else {
     next();
   }
 })
- 
+
 var server = app.listen(3000, function () {
  
   var host = server.address().address;
